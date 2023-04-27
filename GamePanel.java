6697470d3cc5d62e23f09e9467923540e848f5f6
Ömer Edip Aras <edip.aras@ug.bilkent.edu.tr>
2023-04-27 14:57:16 +0300

    final attempt

--- GamePanel.java
+++ GamePanel.java
@@ -1,15 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.ArrayList;
+import java.util.Arrays;
 /**
  * GamePanel
  */
 public class GamePanel extends JPanel {
-    public static JFrame frame = new JFrame();
-    public static int score = 0;
+    private static JFrame frame = new JFrame();
+    private static int score;
+    private static int grayPanelCount = 0;
+    static int depth ;
+
     public GamePanel(int depth){
-        setLayout(new GridLayout(2,2));
+        
+        setLayout(new GridLayout(2,2));   
         if(depth == 0)
         {
             for(int i = 0; i < 4; i++)
@@ -18,69 +22,101 @@ public class GamePanel extends JPanel {
                 add(button);
                 button.addActionListener(new ClickListener());
             }
-            
         }
         else if(depth > 0)
         {
-            
+            // 4 new gamePanels in case depth is positive
             for(int i = 0; i < 4; i++)
             {
-                GamePanel button = new GamePanel(depth - 1);
-                add(button);
-            }
-            
+                GamePanel panel = new GamePanel(depth - 1);
+                // when we have gamePanels that directly involves buttons, check them for same color.
+                if(depth == 1)
+                {
+                    searchForSameColor(panel.getComponents());      
+                }
+                add(panel);
+            }   
+            score = 10;
         }
-        
     }
+    
+    private void searchForSameColor(Component[] components) {
+        GameButton [] buttons = Arrays.copyOf(components, components.length, GameButton[].class);
+        boolean isSame = true;
+        Color color = buttons[0].getBackground();
+        for(int i = 0; i < buttons.length; i++)
+        {
+            if(!color.equals(buttons[i].getBackground()) )
+            {
+                isSame = false; 
+            }
+        }
+        if(isSame)
+        {
+            for(GameButton element: buttons)
+            {
+                element.setBackground(Color.gray);
+                element.setEnabled(false);
+                score += 10;
+                frame.setTitle("Score: " + score);       
+            }
+            grayPanelCount++;  
+        }
+          
+        if(score <= 0 || grayPanelCount == (int) Math.pow(4,depth))
+        {
+            int input = JOptionPane.showConfirmDialog(null
+                , " Would you want to play again?", "Play again?", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+            
+            if(input == JOptionPane.YES_OPTION)
+            {
+                this.removeAll();
+                initializeGame();
+
+            }
+            else if ( input == JOptionPane.NO_OPTION)
+            {
+                System.exit(0);
+            }
+        }
+    }
+
     class ClickListener implements ActionListener{
 
         @Override
         public void actionPerformed(ActionEvent e) {
             if(e.getSource().getClass().getName() == "GameButton")
             {
-                GameButton button =  (GameButton) e.getSource();
+                GameButton button = (GameButton) e.getSource();
                 button.setBackgroundColor();
+                Component [] components = button.getParent().getComponents();
                 // get the clicked button's parent and find all components of it and cast it to GameButton.
-                searchForSameColor((button.getParent().getComponents()));
+                searchForSameColor(components);
+                
                 score--;
                 frame.setTitle("Score: " + score);       
-            }   
+            }
         }
          // if same colors, proceed to make button deactivated and gray
-        public void searchForSameColor(Component [] components){
-            GameButton [] buttons = (GameButton []) components;
-            boolean isSame = true;
-            Color color = buttons[0].getBackground();
-            for(int i = 0; i < buttons.length; i++)
-            {
-                if(color.equals(buttons[i].getBackground()) )
-                {
-                    isSame = false; 
-                }
-
-            }
-            if(isSame)
-            {
-                for(GameButton element: buttons)
-                {
-                    element.setBackground(Color.gray);
-                    element.setEnabled(false);
-                    score += 10;
-                }
-            }
-        }
+          
     }
     public static void main(String[] args) {
-       
-        int score = 0;
-        frame.setTitle("Score: " + score);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600,600);
-        frame.setLocationRelativeTo(null);
-        int depth = 2;
-        frame.add(new GamePanel(depth));
 
-        frame.setVisible(true);
+           
+            frame.setTitle("Score: " + score);
+        
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(600,600);
+            frame.setLocationRelativeTo(null);
+        
+            initializeGame();
+    }
+    public static void initializeGame(){
+            grayPanelCount = 0;
+            score = 10;
+            depth = 2;
+            frame.add(new GamePanel(depth));
+            frame.setVisible(true);
 
     }
 }
